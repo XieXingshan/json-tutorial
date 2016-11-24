af87dca1ff260e18c716a5aa93840e57d5423c01
XieXingshan <15089583364@163.com>
2016-11-24 08:49:13 +0800

    finish SINGULAR

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -34,11 +34,17 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
 
 int lept_parse(lept_value* v, const char* json) {
     lept_context c;
+    int rvalue;
     assert(v != NULL);
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+    if ((rvalue = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
+	lept_parse_whitespace(&c);
+	if (*c.json != '\0')
+	    return LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
+    return rvalue;
 }
 
 lept_type lept_get_type(const lept_value* v) {
